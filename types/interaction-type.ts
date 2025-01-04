85079707289b43ce237beeb62d505b01553ee849
preityzz <106267402+preityzz@users.noreply.github.com>
2025-01-04 14:19:17 +0530

    fix: some linting issue

--- types/interaction-type.ts
+++ types/interaction-type.ts
@@ -1,12 +1,4 @@
-import { Restaurant } from './index';
-// export type Interaction = {
-//   id: number;
-//   restaurantName: string;
-//   interactionDate: string;
-//   interactionType: string;
-//   details: string;
-//   restaurant: Restaurant;
-// };
+
 export interface Interaction {
   id: number;
   interactionType: string;
